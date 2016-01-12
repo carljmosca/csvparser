b0e06ca431e1466902b858cef38a0f89c6a918f4
Carl J. Mosca <carljmosca@gmail.com>
2016-01-11 23:58:19 -0500

    added environment variable for data directory

---
+++ src/main/java/com/moscaville/util/Utility.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.moscaville.util;
+
+/**
+ *
+ * @author moscac
+ */
+public class Utility {
+
+    public static final String DATA_DIRECTORY = "CSVPARSER_DATA_DIRECTORY";
+    
+    public static String getEnvironmentVariable(String name, String defaultValue) {
+        return  System.getProperty(DATA_DIRECTORY) != null ? System.getProperty(DATA_DIRECTORY) : defaultValue;
+    }
+
+}
